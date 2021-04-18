e5c07ed904ffc86d0108164a493f41fa55a236ef
Adrian Sadłocha <adrian@sadlocha.eu>
2021-04-18 03:46:39 +0200

    Use 2 enters instead of 2 spaces

--- keyboards/redox/keymaps/necior/keymap.c
+++ keyboards/redox/keymaps/necior/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_PGDN ,        KC_HOME ,KC_ADEN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTRL,KC_PPLS ,KC_LGUI ,KC_LALT ,     KC_SPC  ,    KC_BSPC ,KC_DEL  ,        KC_DEL  ,KC_ENT  ,    KC_SPC  ,     KC_RALT ,KC_DOWN ,KC_UP   ,KC_RCTRL
+     KC_LCTRL,KC_PPLS ,KC_LGUI ,KC_LALT ,     KC_ENT  ,    KC_BSPC ,KC_DEL  ,        KC_DEL  ,KC_ENT  ,    KC_SPC  ,     KC_RALT ,KC_DOWN ,KC_UP   ,KC_RCTRL
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
